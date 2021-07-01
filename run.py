295bf6aa96885b8b98473f90f62c362c02ab5778
Joe <joebreilly@yahoo.ie>
2021-07-01 20:52:58 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     '''
     Get sales figures input from the user
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers seperated
+    by commas. The loop will repeatedly request data, until it is valid.
     '''
 
     while True:
@@ -25,12 +28,13 @@ def get_sales_data():
 
         data_str = input("Enter your data here: ")
         sales_data = data_str.split(",")
-        validate_data(sales_data)
-
+        
         if validate_data(sales_data):
             print("Data is valid!")
             break
+    
     return sales_data
+
 
 def validate_data(values):
     '''
@@ -43,14 +47,24 @@ def validate_data(values):
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-        )
+                            )
     except ValueError as e:
         print(f"Invalid data {e}, please try again.\n")
         return False
 
     return True
 
-data = get_sales_data
 
+def updata_sales_worksheet(data):
+    '''
+    Update sales worksheet, add new row with the list data provided.
+    '''
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
 
-get_sales_data()
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+updata_sales_worksheet(sales_data)
+
